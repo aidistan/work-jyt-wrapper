f84c631d073aff61137979511e1f5bf7abfe79b6
Aidi Stan <aidistan@live.com>
2018-10-11 23:30:38 +0800

    Catch network errors

--- app/src/main/java/com/bjtcsj/jyt/wrapper/MainActivity.java
+++ app/src/main/java/com/bjtcsj/jyt/wrapper/MainActivity.java
@@ -1,10 +1,14 @@
 package com.bjtcsj.jyt.wrapper;
 
+import android.graphics.Bitmap;
 import android.os.Handler;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -33,8 +37,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
 
-        hideSystemUis();
-        loadTargetWebsite();
+        mBrowserView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+        WebSettings webSettings = mBrowserView.getSettings();
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webSettings.setJavaScriptEnabled(true);
+
+        mBrowserView.setWebViewClient(new WrapperClient());
+        mBrowserView.loadUrl("http://m.jyt.bjtcsj.com");
     }
 
     @Override
@@ -56,53 +70,70 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    protected void hideSystemUis() {
-        mBrowserView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    public void hideWelcome() {
+        mWelcomeView.animate().alpha(0).setDuration(100).withEndAction(new Runnable() {
+            @Override
+            public void run() {
+                mWelcomeView.setVisibility(View.GONE);
+            }
+        });
     }
 
-    protected void loadTargetWebsite() {
-        WebSettings webSettings = mBrowserView.getSettings();
-//        webSettings.setAllowContentAccess(true);
-//        webSettings.setAllowFileAccess(true);
-//        webSettings.setAppCacheEnabled(true);
-//        webSettings.setDomStorageEnabled(true);
-//        webSettings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
-//        webSettings.setLoadWithOverviewMode(true);
-//        webSettings.setLoadsImagesAutomatically(true);
-        webSettings.setJavaScriptEnabled(true);
-//        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-//        webSettings.setSaveFormData(true);
-//        webSettings.setUseWideViewPort(true);
-
-        mBrowserView.loadUrl("http://m.jyt.bjtcsj.com");
-        mBrowserView.setWebViewClient(new WrapperClient());
-    }
-
-    protected void fadeOutWelcomeView() {
-        if (mWelcomeView.getVisibility() != View.VISIBLE) return;
-
+    public void showWarning() {
+        Toast.makeText(MainActivity.this, "网络出错，请检查", Toast.LENGTH_LONG).show();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                mWelcomeView.animate().alpha(0).setDuration(100).withEndAction(new Runnable() {
-                    @Override
-                    public void run() {
-                        mWelcomeView.setVisibility(View.GONE);
-                    }
-                });
+                finish();
             }
-        }, 300);
+        }, 3500);
     }
 
     private class WrapperClient extends WebViewClient {
+        private String mStatus = "created";
+        private Handler mHandler = new Handler();
+        private final Runnable mShowWarning2Runnable = new Runnable() {
+            @Override
+            public void run() {
+                showWarning();
+            }
+        };
+        private final Runnable mHideWelcome2Runnable = new Runnable() {
+            @Override
+            public void run() {
+                hideWelcome();
+            }
+        };
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            mHandler.postDelayed(mShowWarning2Runnable, 10000);
+        }
+
+        @Override
+        public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+            super.onReceivedError(view, request, error);
+            mStatus = "error";
+            showWarning();
+        }
+
+        @Override
+        public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+            super.onReceivedHttpError(view, request, errorResponse);
+            mStatus = "error";
+            showWarning();
+        }
+
         @Override
         public void onPageFinished(WebView view, String url) {
             super.onPageFinished(view, url);
-            fadeOutWelcomeView();
+
+            if (mStatus.equals("created")) {
+                mHandler.postDelayed(mHideWelcome2Runnable,300);
+            }
+
+            mHandler.removeCallbacks(mShowWarning2Runnable);
         }
     }
 }
